56c04430940cf701dcfcf6eab1d5b9a97f31caa6
Daniil Mikhov <daniil.mikhov@nixsolutions.com>
2019-11-19 22:59:00 +0200

    remider function remove static

--- BotTimerFunction/ReminderFunction.cs
+++ BotTimerFunction/ReminderFunction.cs
@@ -5,17 +5,17 @@ using Microsoft.Azure.WebJobs;
 
 namespace BotTimerFunction
 {
-    public static class ReminderFunction
+    public class ReminderFunction
     {
         [FunctionName("ReminderFunction")]
-        public static async Task RunAsync([TimerTrigger("00:01:00")]TimerInfo myTimer)
+        public async Task RunAsync([TimerTrigger("00:01:00")]TimerInfo myTimer)
         {
             var botUrl = Environment.GetEnvironmentVariable("BotUrl");
             var notificationEndpoint = Environment.GetEnvironmentVariable("NotificationEnpoint");
 
             using (HttpClient httpClient = new HttpClient())
             {
-                var response = await httpClient.GetAsync($"{botUrl}/{notificationEndpoint}");
+                await httpClient.GetAsync($"{botUrl}/{notificationEndpoint}");
             }
         }
     }
